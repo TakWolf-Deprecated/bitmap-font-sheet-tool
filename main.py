9705c03b556ae8d1f141ed6ec68d3f20960f07d5
TakWolf <takwolf@foxmail.com>
2023-04-27 11:46:36 +0800

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('main')
+
+
+def main():
+    print('Hello World!')
+
+
+if __name__ == '__main__':
+    main()
